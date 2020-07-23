5ba6a8870c805798ff17376412ff13723dfa7d1f
Michael Koch <m.koch@know-how-akademie.de>
2020-07-23 07:38:18 +0200

    Hinzufügen der Klasse Player im Model mit der ersten Prop. Name

---
+++ TicTacToe/Model/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TicTacToe.ViewModel;
+
+namespace TicTacToe.Model
+{
+    public class Player : TicTacToeViewModelBase
+    {
+        private string _name;
+        public string Name 
+        {
+            get { return _name; }
+            set { SetProperty<string>(ref _name, value); } 
+        }
+
+    }
+}
