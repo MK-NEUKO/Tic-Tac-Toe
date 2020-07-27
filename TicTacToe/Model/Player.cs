1bfa617ebfd04a3c56872a2c7feb5618c32f098b
Michael Koch <m.koch@know-how-akademie.de>
2020-07-27 06:26:25 +0200

    Klasse Player bearbeitet

--- TicTacToe/Model/Player.cs
+++ TicTacToe/Model/Player.cs
@@ -14,5 +14,19 @@ namespace TicTacToe.Model
             set { SetProperty<string>(ref _name, value); } 
         }
 
+        private int _points;
+        public int Points
+        {
+            get { return _points; }
+            set { SetProperty<int>(ref _points, value); }
+        }
+
+        private bool _readyToPlay;
+        public bool ReadyToPlay 
+        { 
+            get { return _readyToPlay; }
+            set {SetProperty<bool>(ref _readyToPlay, value); } 
+        }
+
     }
 }
