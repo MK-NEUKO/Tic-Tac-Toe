e3ac5a7b3012afe778e8a3d1af64c28ee14e9a95
Michael Koch <m.koch@know-how-akademie.de>
2020-07-27 13:46:26 +0200

    Klassen TicTacToeData und Player hinzugefügt

---
+++ TicTacToe/Model/TicTacToeData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TicTacToe.ViewModel;
+
+namespace TicTacToe.Model
+{
+    class TicTacToeData : TicTacToeBase
+    {
+    }
+}
---
+++ TicTacToe/ViewModel/Player.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe.ViewModel
+{
+    class Player : TicTacToeBase
+    {
+
+    }
+}
