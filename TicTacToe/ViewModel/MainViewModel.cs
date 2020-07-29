bb4b6269ee1864b489f5879030382034c82e71a9
Michael Koch <m.koch@know-how-akademie.de>
2020-07-29 13:29:38 +0200

    Programm läuft wieder

--- TicTacToe/ViewModel/MainViewModel.cs
+++ TicTacToe/ViewModel/MainViewModel.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using TicTacToe.Model;
 
 namespace TicTacToe.ViewModel
 {
     class MainViewModel : ViewModelBase
     {
-        Player PlayerXXX = Player {}
+        
 
     }
 }
