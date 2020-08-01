893ecd75c37e91ef42be8163dec8865a7a3ed876
Michael Koch <m.koch@know-how-akademie.de>
2020-08-01 23:09:02 +0200

    GameDisplay, TextBlock - Box per DataBinding - Trigger verbunden

--- TicTacToe/ViewModel/MainViewModel.cs
+++ TicTacToe/ViewModel/MainViewModel.cs
@@ -6,15 +6,28 @@ namespace TicTacToe.ViewModel
 {
     class MainViewModel : ViewModelBase
     {
-        public GameData PlayerX;
-        public GameData PlayerO;
+        private GameData _playerX;
 
+        public GameData PlayerX
+        {
+            get { return _playerX; }
+            set { _playerX = value; }
+        }
+
+        private GameData _playerO;
+
+        public GameData PlayerO
+        {
+            get { return _playerO; }
+            set { _playerO = value; }
+        }
+
+        
         public MainViewModel()
         {
             PlayerX = new GameData { Name = "Hallo X", Points = 12, InAction = true };
             PlayerO = new GameData { Name = "Hallo O", Points = 4, InAction = false };
         }
-
 
     }
 }
