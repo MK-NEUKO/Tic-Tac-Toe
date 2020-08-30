c834b135d5cbdead7631893fd9f47f3430d804d0
Michael Koch <m.koch@know-how-akademie.de>
2020-08-30 23:30:36 +0200

    Gewinner ermitteln funktioniert jetzt. Arbeiten an der Haupmethode PlayTicTacToe.

--- TicTacToe/ViewModel/MainViewModel.cs
+++ TicTacToe/ViewModel/MainViewModel.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Printing;
+﻿using System.Printing;
 using System.Text;
 using System.Windows.Controls;
 using System.Windows.Input;
-using TicTacToe.Model;
 
 namespace TicTacToe.ViewModel
 {
-    class MainViewModel : ViewModelBase
+    partial class MainViewModel : ViewModelBase
     {
         public PlayerData PlayerX { get; set; }
         public PlayerData PlayerO { get; set; }
@@ -34,6 +31,20 @@ namespace TicTacToe.ViewModel
             PlaceASigneCommand = new RelayCommand(PlaceASigneExecute, PlaceASigneCanExecute);
         }
 
+        private void PlayTicTacToe(string name, string signe)
+        {
+            bool winnerX = false;
+            bool winnerO = false;
+            
+            Board.PlaceASigne(name, signe);
+            winnerX = Board.CheckForWinnerX();
+            winnerO = Board.CheckForWinnerO();
+            if (winnerX)
+                PlayerX.Points++;
+            if (winnerO)
+                PlayerO.Points++;            
+        }
+
 
         private bool PlaceASigneCanExecute(object obj)
         {
@@ -50,173 +61,15 @@ namespace TicTacToe.ViewModel
             {
                 if (PlayerX.InAction)
                 {
-                    Board.PlaceASigne(value.Name, "X");
-                    Board.CheckForWinner();
+                    PlayTicTacToe(value.Name, "X");
                     PlayerO.InAction = true;
                     PlayerX.InAction = false;
                 }
                 else
                 {
-                    Board.PlaceASigne(value.Name, "O");
+                    PlayTicTacToe(value.Name, "O");
                     PlayerO.InAction = false;
                     PlayerX.InAction = true;
-                }
-            }
-        }
-
-        //public class GameBoard : Winner
-        //{
-        //    public GameData BoardArea0 { get; set; }
-        //    public GameData BoardArea1 { get; set; }
-        //    public GameData BoardArea2 { get; set; }
-        //    public GameData BoardArea3 { get; set; }
-        //    public GameData BoardArea4 { get; set; }
-        //    public GameData BoardArea5 { get; set; }
-        //    public GameData BoardArea6 { get; set; }
-        //    public GameData BoardArea7 { get; set; }
-        //    public GameData BoardArea8 { get; set; }
-
-        //    List<GameData> BoardAreaList = new List<GameData>();
-
-
-        //    public GameBoard()
-        //    {
-        //        BoardArea0 = new GameData();
-        //        BoardArea1 = new GameData();
-        //        BoardArea2 = new GameData();
-        //        BoardArea3 = new GameData();
-        //        BoardArea4 = new GameData();
-        //        BoardArea5 = new GameData();
-        //        BoardArea6 = new GameData();
-        //        BoardArea7 = new GameData();
-        //        BoardArea8 = new GameData();
-
-        //        BoardAreaList.Add(BoardArea0);
-        //        BoardAreaList.Add(BoardArea1);
-        //        BoardAreaList.Add(BoardArea2);
-        //        BoardAreaList.Add(BoardArea3);
-        //        BoardAreaList.Add(BoardArea4);
-        //        BoardAreaList.Add(BoardArea5);
-        //        BoardAreaList.Add(BoardArea6);
-        //        BoardAreaList.Add(BoardArea7);
-        //        BoardAreaList.Add(BoardArea8);
-        //    }
-
-
-        //    public void PlaceASigne(string name, string signe)
-        //    {
-        //        double index = Char.GetNumericValue(name, 9);
-        //        int i = (int)index;
-        //        BoardAreaList[i].Signe = signe; 
-        //    }
-
-        //    public void ClearGameBoard()
-        //    {
-        //        BoardAreaList.Clear();
-        //    }
-        //}
-
-
-        public class Winner
-        {
-            public GameData BoardArea0 { get; set; }
-            public GameData BoardArea1 { get; set; }
-            public GameData BoardArea2 { get; set; }
-            public GameData BoardArea3 { get; set; }
-            public GameData BoardArea4 { get; set; }
-            public GameData BoardArea5 { get; set; }
-            public GameData BoardArea6 { get; set; }
-            public GameData BoardArea7 { get; set; }
-            public GameData BoardArea8 { get; set; }
-
-            List<GameData> BoardAreaList = new List<GameData>();
-
-
-            public Winner()
-            {
-                BoardArea0 = new GameData();
-                BoardArea1 = new GameData();
-                BoardArea2 = new GameData();
-                BoardArea3 = new GameData();
-                BoardArea4 = new GameData();
-                BoardArea5 = new GameData();
-                BoardArea6 = new GameData();
-                BoardArea7 = new GameData();
-                BoardArea8 = new GameData();
-
-                BoardAreaList.Add(BoardArea0);
-                BoardAreaList.Add(BoardArea1);
-                BoardAreaList.Add(BoardArea2);
-                BoardAreaList.Add(BoardArea3);
-                BoardAreaList.Add(BoardArea4);
-                BoardAreaList.Add(BoardArea5);
-                BoardAreaList.Add(BoardArea6);
-                BoardAreaList.Add(BoardArea7);
-                BoardAreaList.Add(BoardArea8);
-            }
-
-            public void CheckForWinner()
-            {
-                List<GameData> winnerX = BoardAreaList.FindAll(WinnerX);
-                if (winnerX.Count >= 3)
-                {
-                    List<GameData> winnerOne = winnerX.FindAll(WinnerOne);
-                    if (winnerOne.Count == 3)
-                        MakeItGreen(winnerOne);
-
-                    List<GameData> winnerTwo = winnerX.FindAll(WinnerTwo);
-                    if (winnerTwo.Count == 3)
-                        MakeItGreen(winnerTwo);
-                }
-               
-            }
-
-            private void MakeItGreen(List<GameData> winner)
-            {
-                foreach (GameData item in winner)
-                {
-                    item.IsWinner = true;
-                }
-            }
-
-            private bool WinnerX(GameData area)
-            {
-                if (area.Signe == "X")
-                    return true;
-                else
-                    return false;
-            }
-
-            private bool WinnerOne(GameData area)
-            {
-                if (area.IDArea == 0 || area.IDArea == 1 || area.IDArea == 2)
-                    return true;
-                else
-                    return false;
-            }
-
-            private bool WinnerTwo(GameData area)
-            {
-                if (area.IDArea == 3 || area.IDArea == 4 || area.IDArea == 5)
-                    return true;
-                else
-                    return false;
-            }
-
-            public void PlaceASigne(string name, string signe)
-            {
-                double index = Char.GetNumericValue(name, 9);
-                int i = (int)index;
-                BoardAreaList[i].Signe = signe;
-                BoardAreaList[i].IDArea = i;
-            }
-
-            public void ClearGameBoard()
-            {
-                foreach (GameData item in BoardAreaList)
-                {
-                    item.Signe = null;
-                    item.IsWinner = false;
                 }
             }
         }
